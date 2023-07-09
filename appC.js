323ac58d20867ed1554ccf5f9534b76648fb5884
cassiemayer <cassiemayer@yahoo.com>
2023-07-09 17:20:59 -0400

    push

--- appC.js
+++ appC.js
@@ -9,9 +9,45 @@
     //bubble
 
     //needle gauge1
+    let datagauge = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: #dependent on dropdown,
+            title: { text: "Avg Ozone AQI" },
+            type: "indicator",
+            mode: "gauge+number"
+        }
+    ];
+    
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('myDiv', datagauge, layout);
+    
     //needle gauge2
+    let datagauge = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: #dependent on dropdown,
+            title: { text: "Avg NO2 AQI" },
+            type: "indicator",
+            mode: "gauge+number"
+        }
+    ];
+    
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('myDiv', datagauge, layout);
     //needle gauge3
-
+    let datagauge = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: #dependent on dropdown,
+            title: { text: "Avg PM2.5 AQI" },
+            type: "indicator",
+            mode: "gauge+number"
+        }
+    ];
+    
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('myDiv', datagauge, layout);
 //end- no data after this point
 
 //build MetaData function
